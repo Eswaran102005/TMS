1074f1321f7d593a10e9887f544dbb67fc40f0c2
Macbook <macbook@MacBook-Pro.local>
2026-05-21 23:04:50 +0530

    updated api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'https://tms-s77c.onrender.com';
 
 const getToken = () => {
   return localStorage.getItem('token');
